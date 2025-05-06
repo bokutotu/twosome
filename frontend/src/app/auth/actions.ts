9818c2624b28dea1aa7e82f537a4e61636374662
bokutotu <mushin.hudoushin@gmail.com>
2025-05-07 06:02:18 +0900

    google

--- frontend/src/app/auth/actions.ts
+++ frontend/src/app/auth/actions.ts
@@ -1,46 +1,72 @@
 'use server'
 
 import { supabaseServer } from '@/auth/supabase'
+import { Provider } from '@supabase/supabase-js'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 
 export async function login(formData: FormData) {
-  const supabase = await supabaseServer()
+    const supabase = await supabaseServer()
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
+    // type-casting here for convenience
+    // in practice, you should validate your inputs
+    const data = {
+        email: formData.get('email') as string,
+        password: formData.get('password') as string,
+    }
 
-  const { error } = await supabase.auth.signInWithPassword(data)
+    const { error } = await supabase.auth.signInWithPassword(data)
 
-  if (error) {
-    redirect('/error')
-  }
+    if (error) {
+        redirect('/error')
+    }
 
-  revalidatePath('/', 'layout')
-  redirect('/')
+    revalidatePath('/', 'layout')
+    redirect('/')
 }
 
 export async function signup(formData: FormData) {
-  const supabase = await supabaseServer()
+    const supabase = await supabaseServer()
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
+    // type-casting here for convenience
+    // in practice, you should validate your inputs
+    const data = {
+        email: formData.get('email') as string,
+        password: formData.get('password') as string,
+    }
 
-  const { error } = await supabase.auth.signUp(data)
+    const { error } = await supabase.auth.signUp(data)
 
-  if (error) {
-    redirect('/error')
-  }
+    if (error) {
+        redirect('/error')
+    }
 
-  revalidatePath('/', 'layout')
-  redirect('/')
+    revalidatePath('/', 'layout')
+    redirect('/')
+}
+
+export async function loginWithProvider(formData: FormData) {
+    const supabase = await supabaseServer()
+    const provider = formData.get('provider') as Provider
+
+    const redirectTo = `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`
+    console.log('Redirecting to:', redirectTo)
+
+    const { error, data } = await supabase.auth.signInWithOAuth({
+        provider: provider,
+        options: {
+            redirectTo,
+        }
+    })
+
+    if (error) {
+        redirect('/error')
+    }
+
+    if (data?.url) {
+        redirect(data.url)
+    }
+
+    redirect('/')
 }
--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,14 +1,26 @@
-import { login, signup } from './actions'
+import { login, loginWithProvider, signup } from './actions'
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={login}>Log in</button>
+        <button formAction={signup}>Sign up</button>
+      </form>
+
+      <form action={loginWithProvider}>
+        <label htmlFor="provider">Provider:</label>
+        <select id="provider" name="provider">
+          <option value="github">GitHub</option>
+          <option value="google">Google</option>
+          <option value="twitter">Twitter</option>
+        </select>
+        <button type="submit">Log in with provider</button>
+      </form>
+    </div>
   )
 }
