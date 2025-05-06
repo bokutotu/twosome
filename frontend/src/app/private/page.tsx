183bc3d5e292fd53c9ee3f2c650049ab23ef1c58
bokutotu <mushin.hudoushin@gmail.com>
2025-05-06 15:31:42 +0900

    supabaseのコピペ

---
+++ frontend/src/app/private/page.tsx
@@ -0,0 +1,14 @@
+import { supabaseServer } from '@/auth/supabase'
+import { redirect } from 'next/navigation'
+
+
+export default async function PrivatePage() {
+  const supabase = await supabaseServer()
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/login')
+  }
+
+  return <p>Hello {data.user.email}</p>
+}
